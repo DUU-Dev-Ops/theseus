af52c273a4a0db1e33fc0b72ccd6a75697521534
Amy Yang <jy152@duke.edu>
2016-01-08 21:06:01 +0800

    fixed code in comparisonController

--- public/js/comparisonController.js
+++ public/js/comparisonController.js
@@ -101,22 +101,3 @@ comparisonController.controller('comparisonController',['$scope', '$routeParams'
         }
     }
 }]);
-
-
-app.directive('datepicker', function() {
-    return {
-        restrict: 'A',
-        require : 'ngModel',
-        link : function (scope, element, attrs, ngModelCtrl) {
-            $(function(){
-                element.datepicker({
-                    dateFormat:'mm/dd/yy',
-                    onSelect:function (date) {
-                        ngModelCtrl.$setViewValue(date);
-                        scope.$apply();
-                    }
-                });
-            });
-        }
-    }
-});
