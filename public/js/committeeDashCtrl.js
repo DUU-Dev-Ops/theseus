cc2a5e78d3121cff2104a7f9d09a72b380634406
Amy Yang <jy152@duke.edu>
2016-02-12 22:34:53 -0500

    added sample nvd3 graph to attendence analysis panel

--- public/js/committeeDashCtrl.js
+++ public/js/committeeDashCtrl.js
@@ -19,6 +19,7 @@ committeeDashCtrl.controller('committeeDashCtrl',['$scope','$http','$routeParams
             $scope.errorGetEvent = true;
   });
     
+
     // Add selected event to activeEvents before making graph
   
     $scope.select = function(event){
@@ -37,38 +38,67 @@ committeeDashCtrl.controller('committeeDashCtrl',['$scope','$http','$routeParams
     
     // Make new graph based on event elected. Can now only make graph for one event at a time.
     $scope.showGraph = function(){
-        var graphDiv = document.getElementById("graphdiv");
-        // clear previous graph
-        graphDiv.innerHTML = "";
-        var eventid = $scope.activeEvents[0]._id;
-        var path = '../swipe_data/'+eventid+'.csv';
+        //    Configure NVD3 Line Graph
+                nv.addGraph(function() {
+          var chart = nv.models.lineChart()
+                        .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+                        .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+//                        .transitionDuration(350)  //how fast do you want the lines to transition?
+                        .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+                        .showYAxis(true)        //Show the y-axis
+                        .showXAxis(true)        //Show the x-axis
+          ;
+
+          chart.xAxis     //Chart x-axis settings
+              .axisLabel('Time (ms)')
+              .tickFormat(d3.format(',r'));
+
+          chart.yAxis     //Chart y-axis settings
+              .axisLabel('Voltage (v)')
+              .tickFormat(d3.format('.02f'));
+
+          /* Done setting the chart up? Time to render it!*/
+          var myData = sinAndCos();   //You need data...
+
+          d3.select('#chart svg')    //Select the <svg> element you want to render the chart in.   
+              .datum(myData)         //Populate the <svg> element with chart data...
+              .call(chart);          //Finally, render the chart!
+
+          //Update the chart when window resizes.
+          nv.utils.windowResize(function() { chart.update() });
+          return chart;
+        });
+         
+         
+//        var eventid = $scope.activeEvents[0]._id;
+//        var path = '../swipe_data/'+eventid+'.csv';
         
         // Test if file exists
-        $http({
-            method:'GET',
-            url: path
-        }).then(function successCallBack(response){
-            // File exists, make graph
-            $scope.fileDNE.status = false;
-            $scope.hasGraph = true;
-            var g = new Dygraph(graphDiv,path,{
-            legend: 'always',
-            title:'Swipe Frequency vs. Time',
-            showRoller: false,
-            ylabel:'Swipe Freq',
-            showRangeSelector: true,
-            rangeSelectorHeight: 30,
-            rangeSelectorPlotStrokeColor: 'yellow',
-            rangeSelectorPlotFillColor: 'lightyellow'
-        });
-        }, function errorCallBack(response){
-            // File doesn't exist
-            $scope.hasGraph = false;
-            $scope.fileDNE.status = true;
-            $scope.fileDNE.file = $scope.activeEvents[0].event_name;
-        });
+//        $http({
+//            method:'GET',
+//            url: path
+//        }).then(function successCallBack(response){
+//            // File exists, make graph
+//            $scope.fileDNE.status = false;
+//            $scope.hasGraph = true;
+//            var g = new Dygraph(graphDiv,path,{
+//            legend: 'always',
+//            title:'Swipe Frequency vs. Time',
+//            showRoller: false,
+//            ylabel:'Swipe Freq',
+//            showRangeSelector: true,
+//            rangeSelectorHeight: 30,
+//            rangeSelectorPlotStrokeColor: 'yellow',
+//            rangeSelectorPlotFillColor: 'lightyellow'
+//        });
+//        }, function errorCallBack(response){
+//            // File doesn't exist
+//            $scope.hasGraph = false;
+//            $scope.fileDNE.status = true;
+//            $scope.fileDNE.file = $scope.activeEvents[0].event_name;
+//        });
     };
-    
+//    end make new graph
 }]);
 
 committeeDashCtrl.run(function(DTDefaultOptions){
@@ -87,3 +117,38 @@ committeeDashCtrl.filter('dateFormat', function($filter)
 
  };
 });
+
+/**************************************
+ * Simple test data generator
+ */
+function sinAndCos() {
+  var sin = [],sin2 = [],
+      cos = [];
+
+  //Data is represented as an array of {x,y} pairs.
+  for (var i = 0; i < 100; i++) {
+    sin.push({x: i, y: Math.sin(i/10)});
+    sin2.push({x: i, y: Math.sin(i/10) *0.25 + 0.5});
+    cos.push({x: i, y: .5 * Math.cos(i/10)});
+  }
+
+  //Line chart data should be sent as an array of series objects.
+  return [
+    {
+      values: sin,      //values - represents the array of {x,y} data points
+      key: 'Sine Wave', //key  - the name of the series.
+      color: '#ff7f0e'  //color - optional: choose your own line color.
+    },
+    {
+      values: cos,
+      key: 'Cosine Wave',
+      color: '#2ca02c'
+    },
+    {
+      values: sin2,
+      key: 'Another sine wave',
+      color: '#7777ff',
+      area: true      //area - set to true if you want this line to turn into a filled area chart.
+    }
+  ];
+}
