17c20b3d4ed486f4244b65fa6690ea50b54e612b
Vishnu Menon <me@vishnumenon.com>
2016-08-16 17:54:39 -0400

    Fixed committee pictures and added realtime data updates

--- config/routes.js
+++ config/routes.js
@@ -4,16 +4,16 @@
 
 var express = require('express');
 
-module.exports = function(app) {
-  var hello = require('../api/hello');
-  var events = require('../api/events.js');
-  app.get('/', hello.world);
-  app.post('/api/events', events.create);
-  app.get('/api/events')
-  app.get('/api/events/:committee', events.find);  
-  app.get('/api/event/:id', events.findByID);
-  app.post('/api/event/:id/attendee', events.addAttendee);
-  app.post('/api/event/:id/swipe', events.addSwipe);
-  app.post('/api/event/:id/update',events.update);
-  // TODO: Add more
+module.exports = function(app, io) {
+    var hello = require('../api/hello');
+    var events = require('../api/events.js')(io);
+    app.get('/', hello.world);
+    app.post('/api/events', events.create);
+    app.get('/api/events')
+    app.get('/api/events/:committee', events.find);
+    app.get('/api/event/:id', events.findByID);
+    app.post('/api/event/:id/attendee', events.addAttendee);
+    app.post('/api/event/:id/swipe', events.addSwipe);
+    app.post('/api/event/:id/update', events.update);
+    // TODO: Add more
 };
--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 // Set up Express.js
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
 
 // Load libraries
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var favicon = require('serve-favicon');
+var io = require('socket.io')(http);
 
 // Set up logging
 app.use(morgan('dev'));
@@ -31,29 +33,35 @@ require('./config/db')();
 // TODO: Configure and load passport.js
 
 // Set up app routes
-require('./config/routes')(app);
+require('./config/routes')(app, io);
 
 // TODO: Set up CSURF, maybe?
 
 // Default error handler
 app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  switch(err.name) {
-  	case "ValidationError":
-  	case "CastError":
-  		err.status = 400;
-  		break;
-  	// Add further specific errors here
-  }
-  res.status(err.status || 500).send({error: err});
+    console.error(err.stack);
+    switch (err.name) {
+        case "ValidationError":
+        case "CastError":
+            err.status = 400;
+            break;
+            // Add further specific errors here
+    }
+    res.status(err.status || 500).send({ error: err });
 });
 
 exports = module.exports = app;
 if (!module.parent) {
-  var port = process.env.PORT || 8080; // 8080 as default
-  // On Linux make sure you have root to open port 80
-  app.listen(port, function() {
-    console.log('Listening on port ' + port);
-  });
-}
+    var port = process.env.PORT || 8080; // 8080 as default
 
+    io.on('connection', function(socket) {
+        socket.on('room', function(room) {
+            socket.join(room);
+        });
+    })
+
+    // On Linux make sure you have root to open port 80
+    http.listen(port, function() {
+        console.log('Listening on port ' + port);
+    });
+}
